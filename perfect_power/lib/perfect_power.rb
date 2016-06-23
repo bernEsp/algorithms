1b3737025759aacc73d65b3715cb52a428ce8e44
Bernardo Galindo <bernardo466@gmail.com>
2016-06-23 14:04:37 -0500

    add perfect power exercise

---
+++ perfect_power/lib/perfect_power.rb
@@ -0,0 +1,104 @@
+require 'prime'
+class PerfectPower
+  def isPP(n)
+    divisors = _divisors(n)
+    divisors = divisors.delete_if{|x| x== 1 || x == n}.compact
+    pr = perfect_powers(divisors, n)
+    perfect_power_response(pr)
+  end
+
+  def divisors(n)
+    divisors ||= _divisors(n).sort
+  end
+
+  def isPPv2(n)
+    # n ** 0.5 = n ^ 1/2 = Math.sqrt n
+    # reduce the list of divisors until the sqrt of n
+    # considering only the n ** 0.5 first divisors
+    (2 .. (n ** 0.5)).each do |m|
+      k = (Math.log(n) / Math.log(m)).round
+      return [m,k] if (m ** k == n )
+    end
+    return nil
+  end
+
+  def isPPv3(n)
+    2.upto(n ** 0.5) do |m|
+      # get all k valid for log of n with base m
+      # when k is a float without decimals or zero
+      # m ** k == n condition will be true
+      k = Math.log(n, m).round
+      return [m,k] if (m ** k == n)
+    end
+    return nil
+  end
+
+  private
+  def perfect_powers(divisors, n)
+    max_k = Math.log2(n)
+    perfect_powers = []
+    1.upto(max_k) do |k|
+      result = divisors.select{|div| div ** k == n}
+      unless result.empty?
+        perfect_powers = [result.first, k]
+        break
+      end
+    end
+    perfect_powers
+  end
+
+  def perfect_power_response(perfect_powers)
+    return perfect_powers unless perfect_powers.empty?
+  end
+
+  def factorization(n, primes)
+    pos = 0
+    result = []
+    while n != 1 
+      break if pos >= primes.size
+      if n % primes[pos] == 0
+        n = n / primes[pos]
+        result << primes[pos]
+      else
+        pos += 1
+      end
+    end
+    result
+  end
+
+  def _divisors(n)
+    primes = Prime.first 10
+    factors = factorization(n, primes)
+    factor_powers = factor_powers(factors)
+    # {2=>4, 3=>2, 5=>1}
+    small_factor = factor_powers.keys.first
+    factor_power_values = factor_powers.each_pair.collect do |prime, power|
+      factor_power_values(prime, power)
+    end
+    # [[1,2,4,8,16], [1,3,9], [5]]
+    row_divisors = [factor_power_values[0]]
+    factor_power_values[1..-1]&.each do |fpv|
+      tmp_row_divisors = []
+      row_divisors.each do |values|
+        fpv.each do |v|
+          next if v == 1
+          tmp_row_divisors << values.collect { |vs| vs * v }
+        end
+      end
+      row_divisors << tmp_row_divisors.flatten
+    end
+    row_divisors.flatten
+  end
+
+  def factor_powers(divisors)
+    result = {}
+    divisors.each do |div|
+      result[div] = divisors.count(div)
+    end
+    result
+  end
+
+  def factor_power_values(prime, power)
+    (0..power).collect{|p| prime ** p }
+  end
+end
---
+++ perfect_power/test/perfect_power_test.rb
@@ -0,0 +1,74 @@
+require 'minitest/autorun'
+require './lib/perfect_power'
+
+class PerfectPowerTest < Minitest::Test
+  def setup
+    @perfect_power = PerfectPower.new
+  end
+
+  def test_be_valid_for_lower_numbers
+    assert_equal [2,2], @perfect_power.isPP(4)
+    assert_equal [6,2], @perfect_power.isPP(36)
+    assert_equal [3,2], @perfect_power.isPP(9) 
+    assert_equal nil, @perfect_power.isPP(5)
+  end
+
+  def test_work_with_random_values
+    1.upto(100) do |x|
+      a = rand(1000..65000)
+      r = @perfect_power.isPP(a)
+      if !r.nil? && (r[0] ** r[1]) != a
+        assert_equal r, a
+        break
+      end
+    end
+  end
+
+  def test_validate_perfect_power_of_27
+    assert_equal [3, 3], @perfect_power.isPP(27)
+  end
+
+  def test_be_valid_for_lower_numbers_v2
+    assert_equal [2,2], @perfect_power.isPPv2(4)
+    assert_equal [6,2], @perfect_power.isPPv2(36)
+    assert_equal [3,2], @perfect_power.isPPv2(9) 
+    assert_equal nil, @perfect_power.isPPv2(5)
+  end
+
+  def test_v2_should_work_with_random_values
+    1.upto(100) do |x|
+      a = rand(1000..65000)
+      r = @perfect_power.isPPv2(a)
+      if !r.nil? && (r[0] ** r[1]) != a
+        assert_equal r, a
+        break
+      end
+    end
+  end
+
+  def test_be_valid_for_lower_numbers_v3
+    assert_equal [2,2], @perfect_power.isPPv3(4)
+    assert_equal [6,2], @perfect_power.isPPv3(36)
+    assert_equal [3,2], @perfect_power.isPPv3(9) 
+    assert_equal nil, @perfect_power.isPPv3(5)
+  end
+
+  def test_v3_should_work_with_random_values
+    1.upto(100) do |x|
+      a = rand(1000..65000)
+      r = @perfect_power.isPPv3(a)
+      if !r.nil? && (r[0] ** r[1]) != a
+        assert_equal r, a
+        break
+      end
+    end
+  end
+
+  def test_return_a_list_of_all_divisors
+    assert_equal [1,2], @perfect_power.divisors(2)
+  end
+
+  def test_return_all_divisors_of_360
+    assert_equal [1,2,3,4,5,6,8,9,10,12,15,18,20, 24,30,36,40,45,60,72,90,120,180,360].sort, @perfect_power.divisors(360)
+  end
+end
